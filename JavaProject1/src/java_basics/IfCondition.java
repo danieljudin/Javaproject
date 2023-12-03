a7093cc846985d1e42d39647b39124eee782ba31
Rad <kalmanjudin2000@gmail.com>
2023-12-03 20:54:56 +0200

    added new Task: IfCondition

---
+++ JavaProject1/src/java_basics/IfCondition.java
@@ -0,0 +1,18 @@
+package java_basics;
+
+public class IfCondition {
+
+    // Видео: https://www.youtube.com/watch?v=ryR033ld_N0&ab_channel=alishev
+
+    // Напиши следующую программу:
+    /*
+        1. Используй 2 переменных типа int (x и y)
+            x равен 5, а y равен 3
+        2. Напиши следующие условие
+           2.1. Если x больше y, то выведи в консоль: "X больше Y"
+           2.2. Иначе выведи в консоль: "X меньше Y"
+     */
+    public static void main(String[] args) {
+
+    }
+}
